5616245e4d94c3809119213cbb3475978afa1721
Kyle Fuller <kyle@fuller.li>
2017-12-24 18:16:46 -0800

    wip

--- Package.swift
+++ Package.swift
@@ -1,4 +1,3 @@
-// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
@@ -13,5 +12,5 @@ let package = Package(
     .target(name: "PathKit", dependencies: [], path: "Sources"),
     .testTarget(name: "PathKitTests", dependencies: ["PathKit", "Spectre"], path:"Tests/PathKitTests")
   ],
-  swiftLanguageVersions: [3]
+  swiftLanguageVersions: [3, 4]
 )
