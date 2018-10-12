8d52514aff8a19dbce9b252c386ebe985b0bacbe
Pedro Piñera <pepibumur@gmail.com>
2018-10-12 09:05:02 +0200

    Change minimum deployment target

--- Project.swift
+++ Project.swift
@@ -1,6 +1,9 @@
 import ProjectDescription
 
 let project = Project(name: "PathKit-carthage",
+                      settings: Settings(base: [
+                        "MACOSX_DEPLOYMENT_TARGET": "10.9"
+                    ]),
                       targets: [
                         Target(name: "PathKit",
                                platform: .macOS,
